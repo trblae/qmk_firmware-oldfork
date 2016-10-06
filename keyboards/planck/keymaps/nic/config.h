485a7da3b77b3690e8a00d5cf8ff2322198a1d0e
Nic Aitch <nic@nicinabox.com>
2016-10-06 00:56:54 -0500

    Update nic keymap to support rgb underglow

---
+++ keyboards/planck/keymaps/nic/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define RGB_DI_PIN B2     // The pin your RGB strip is wired to
+#define RGBLIGHT_TIMER
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 10
+#define RGBLIGHT_VAL_STEP 10
+
+#endif
