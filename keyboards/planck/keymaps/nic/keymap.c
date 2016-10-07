4721698847c010379708bf0b354de2591e9d3fc9
Nic Aitch <nic@nicinabox.com>
2016-10-07 18:50:36 -0500

    Add RGUI

--- keyboards/planck/keymaps/nic/keymap.c
+++ keyboards/planck/keymaps/nic/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,            KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC},
   {CTL_T(KC_ESC),     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT},
   {TD(SFT_CAPS),      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  MT(MOD_RSFT, KC_ENT)},
-  {TD(TD_UNDERGLOW),  KC_MEH,   KC_LALT,  KC_LGUI,  FUNC(1),  FUNC(3),  FUNC(3),  FUNC(2),  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT}
+  {TD(TD_UNDERGLOW),  KC_MEH,   KC_LALT,  KC_LGUI,  FUNC(1),  FUNC(3),  FUNC(3),  FUNC(2),  MT(MOD_RGUI, KC_LEFT),  KC_DOWN,  KC_UP,    KC_RGHT}
 },
 
 [UPPER_LAYER] = {
