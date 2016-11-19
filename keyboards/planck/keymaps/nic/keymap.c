f8f4444a86171887fb33e6f56530f5385b08c411
Nic Aitch <nic@nicinabox.com>
2016-11-19 15:13:18 -0600

    Update planck keymap

--- keyboards/planck/keymaps/nic/keymap.c
+++ keyboards/planck/keymaps/nic/keymap.c
@@ -13,6 +13,7 @@
 enum {
   SFT_CAPS = 0,
   TD_UNDERGLOW = 1,
+  VOLD_MUTE = 2,
 };
 
 // Macros
@@ -27,49 +28,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,            KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC},
   {CTL_T(KC_ESC),     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT},
   {TD(SFT_CAPS),      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  MT(MOD_RSFT, KC_ENT)},
-  {TD(TD_UNDERGLOW),  KC_MEH,   KC_LALT,  KC_LGUI,  FUNC(1),  FUNC(3),  FUNC(3),  FUNC(2),  MT(MOD_RGUI, KC_LEFT),  KC_DOWN,  KC_UP,    KC_RGHT}
+  {TD(TD_UNDERGLOW),  KC_MEH,   KC_LALT,  KC_LGUI,  F(1),     F(3),     F(3),     F(2),  MT(MOD_RGUI, KC_LEFT),  KC_DOWN,  KC_UP,    KC_RGHT}
 },
 
 [UPPER_LAYER] = {
-  {KC_GRV,    KC_1,       KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______},
-  {_______,   KC_4,       KC_5,     KC_6,     KC_DOT,   _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS},
-  {_______,   KC_7,       KC_8,     KC_9,     KC_0,     _______,  _______,  _______,  _______,  KC_MUTE,  _______,  _______},
-  {FUNC(6),   KC_HYPR,    _______,  _______,  _______,  _______,  _______,  _______,  KC_MPLY,  KC_VOLD,  KC_VOLU,  KC_MFFD}
+  {KC_GRV,                KC_1,       KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,           KC_0,     _______},
+  {_______,               KC_4,       KC_5,     KC_6,     KC_DOT,   _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,        KC_RBRC,  KC_BSLS},
+  {_______,               KC_7,       KC_8,     KC_9,     KC_0,     _______,  _______,  _______,  _______,  KC_DOT,        _______,  _______},
+  {TG(COLEMAK_LAYER),     KC_HYPR,    _______,  _______,  _______,  _______,  _______,  _______,  KC_MPLY,  TD(VOLD_MUTE),  KC_VOLU,  KC_MFFD}
 },
 
 [LOWER_LAYER] = {
-  {KC_TILD,  KC_EXLM,   KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  _______},
-  {_______,  KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE},
-  {_______,  KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  _______},
-  {FUNC(4),  KC_HYPR,   _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END}
+  {KC_TILD,         KC_EXLM,   KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  _______},
+  {_______,         KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE},
+  {_______,         KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  _______},
+  {TG(GAME_LAYER),  KC_HYPR,   _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END}
 },
 
 [SPACEFN_LAYER] = {
-  {_______, _______,  M(M_USERNAME),    _______, LGUI(LSFT(KC_4)), LGUI(LCTL(LSFT(KC_4))),  _______, KC_HOME,           KC_UP,   KC_END,   _______, KC_DELETE},
-  {_______, _______,  M(M_RANDDIGIT),   _______, _______,           _______,                KC_PGUP, KC_LEFT,           KC_DOWN, KC_RIGHT, _______, _______},
-  {_______, _______,  M(M_RANDLETTER),  _______, _______,           _______,                KC_PGDN, LGUI(LSFT(KC_M)),  _______, _______,  _______, RESET},
-  {FUNC(5), FUNC(0),  _______,          _______, _______,           _______,                _______, _______,           _______, _______,  _______, _______}
+  {_______,           _______,  M(M_USERNAME),    _______, LGUI(LSFT(KC_4)), LGUI(LCTL(LSFT(KC_4))),  _______, KC_HOME,           KC_UP,   KC_END,   _______, KC_DELETE},
+  {_______,           _______,  M(M_RANDDIGIT),   _______, _______,           _______,                KC_PGUP, KC_LEFT,           KC_DOWN, KC_RIGHT, _______, _______},
+  {_______,           _______,  M(M_RANDLETTER),  _______, _______,           _______,                KC_PGDN, LGUI(LSFT(KC_M)),  _______, _______,  _______, RESET},
+  {TG(TENKEY_LAYER),  F(0),     _______,          _______, _______,           _______,                _______, _______,           _______, _______,  _______, _______}
 },
 
 [TENKEY_LAYER] = {
   {_______, _______,  _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS, KC_BSPC},
   {_______, _______,  _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, KC_NLCK},
   {_______, _______,  _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PDOT, KC_ENT},
-  {FUNC(0), _______,  _______, _______, _______, KC_SPC,  KC_SPC,  KC_KP_0, _______, _______, _______, _______}
+  {F(0),    _______,  _______, _______, _______, KC_SPC,  KC_SPC,  KC_KP_0, _______, _______, _______, _______}
 },
 
 [GAME_LAYER] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______},
-  {FUNC(0), _______, _______, _______, _______, _______, _______, _______, KC_SLSH, KC_LEFT, KC_DOWN, KC_RIGHT}
+  {F(0),    _______, _______, _______, _______, _______, _______, _______, KC_SLSH, KC_LEFT, KC_DOWN, KC_RIGHT}
 },
 
 [COLEMAK_LAYER] = {
   {_______,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  _______},
   {_______,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT},
   {_______,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  _______,  KC_ENT},
-  {FUNC(0),  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {F(0),     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 [UNDERGLOW_LAYER] = {
@@ -91,10 +92,6 @@ const uint16_t PROGMEM fn_actions[] = {
 
     // Tap for space, hold for SpaceFN
     [3] = ACTION_LAYER_TAP_KEY(SPACEFN_LAYER, KC_SPC),
-
-    [4] = ACTION_LAYER_TOGGLE(GAME_LAYER),
-    [5] = ACTION_LAYER_TOGGLE(TENKEY_LAYER),
-    [6] = ACTION_LAYER_TOGGLE(COLEMAK_LAYER),
 };
 
 void underglow_tapdance (qk_tap_dance_state_t *state, void *user_data) {
@@ -118,7 +115,8 @@ void underglow_tapdance_reset (qk_tap_dance_state_t *state, void *user_data) {
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [SFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-  [TD_UNDERGLOW] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, underglow_tapdance, underglow_tapdance_reset)
+  [TD_UNDERGLOW] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, underglow_tapdance, underglow_tapdance_reset),
+  [VOLD_MUTE] = ACTION_TAP_DANCE_DOUBLE(KC_VOLD, KC_MUTE)
 };
 
 // This bit of logic seeds a wee linear congruential random number generator
