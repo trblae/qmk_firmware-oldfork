d99cb78530cca5f11398ed344316f2fe7b0a8d27
Nic Aitch <nic@nicinabox.com>
2016-12-30 16:53:51 -0600

    Fix planck animations

--- keyboards/planck/keymaps/nic/config.h
+++ keyboards/planck/keymaps/nic/config.h
@@ -9,6 +9,7 @@
 
 #define RGB_DI_PIN B2     // The pin your RGB strip is wired to
 #define RGBLIGHT_TIMER
+#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 12
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 10
