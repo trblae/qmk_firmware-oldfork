a1f0d9e4a9354b18b947f5672d0bdca8f13e3583
Nic Aitch <nic@nicinabox.com>
2017-01-07 19:56:56 -0600

    Tweak minivan (nic)

--- keyboards/tv44/keymaps/nic/keymap.c
+++ keyboards/tv44/keymaps/nic/keymap.c
@@ -4,9 +4,9 @@
 
 extern keymap_config_t keymap_config;
 
-#define _QW 0
-#define _L1 1
-#define _L2 2
+#define QWERTY 0
+#define LOWER 1
+#define UPPER 2
 
 enum {
   SFT_CAPS = 0,
@@ -14,34 +14,34 @@ enum {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = {
+  [QWERTY] = {
     {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
     {CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
-    {TD(SFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, MT(MOD_RSFT, KC_ENT) },
-    {MO(_L1), KC_LALT, KC_LGUI, F(1),  XXXXXXX, XXXXXXX, XXXXXXX, F(2),  KC_RGUI, KC_LEFT,  KC_DOWN,  KC_RIGHT }
+    {TD(SFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_ENTER },
+    {MO(LOWER), KC_LALT, KC_LGUI, F(1),  XXXXXXX, XXXXXXX, XXXXXXX, F(2),  KC_RGUI, KC_LEFT,  KC_DOWN,  KC_RIGHT }
   },
-  [_L1] = {
+  [LOWER] = {
     {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL  },
-    {_______, _______, _______, _______, _______,  _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS },
-    {_______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+    {_______, _______, _______, _______, _______,  _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE },
+    {_______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, LSFT(KC_SLSH), _______ },
     {_______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______ }
   },
-  [_L2] = {
+  [UPPER] = {
     {_______, KC_1,  KC_2,  KC_3,   KC_4,   KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL },
-    {_______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE  },
-    {_______, _______, _______, _______, _______, _______, _______,    _______,    _______,    _______,    KC_SLSH, KC_VOLU },
-    {_______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_MPLY, KC_MPRV, KC_MNXT, TD(VOLD_MUTE) }
+    {_______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS  },
+    {_______, _______, _______, _______, _______, _______, _______,    _______,    _______,    _______,  KC_SLSH, KC_VOLU },
+    {_______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD }
   }
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-  [0] = ACTION_DEFAULT_LAYER_SET(_QW),
+  [0] = ACTION_DEFAULT_LAYER_SET(QWERTY),
 
-  // Tap for backspace, hold for L1
-  [1] = ACTION_LAYER_TAP_KEY(_L1, KC_BSPC),
+  // Tap for enter, hold for L1
+  [1] = ACTION_LAYER_TAP_KEY(LOWER, KC_ENTER),
 
   // Tap for space, hold for L2
-  [2] = ACTION_LAYER_TAP_KEY(_L2, KC_SPC),
+  [2] = ACTION_LAYER_TAP_KEY(UPPER, KC_SPC),
 };
 
 void persistant_default_layer_set(uint16_t default_layer) {
