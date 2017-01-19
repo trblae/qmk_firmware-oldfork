a1624a1ce51496bce4828ad04a56bb15f614cd4b
Nic Aitch <nic@nicinabox.com>
2017-01-19 10:43:22 -0600

    Add lets split nic keymap (wip)

---
+++ keyboards/lets_split/keymaps/nic/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
+#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
+
+#define TAPPING_TERM 200
+
+#define randadd 53
+#define randmul 181
+#define randmod 167
+
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+#endif
