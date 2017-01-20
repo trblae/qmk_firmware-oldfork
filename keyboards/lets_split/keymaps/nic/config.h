3f40b42fd56bbc6fcd70bea921a32878561429e7
Nic Aitch <nic@nicinabox.com>
2017-01-20 16:10:06 -0600

    Explicitly require rev2 config for now

--- keyboards/lets_split/keymaps/nic/config.h
+++ keyboards/lets_split/keymaps/nic/config.h
@@ -1,14 +1,7 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#include "../../config.h"
-
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 6
-#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
-
-#define USE_SERIAL
+#include "../../rev2/config.h"
 
 #define TAPPING_TERM 200
 
