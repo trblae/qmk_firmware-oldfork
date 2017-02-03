54baa9906c44e691d2ecee922358d3b8cbab05d3
Nic Aitch <nic@nicinabox.com>
2017-02-03 09:32:15 -0600

    Set Shift Up be question mark

--- keyboards/tv44/keymaps/nic/keymap.c
+++ keyboards/tv44/keymaps/nic/keymap.c
@@ -4,6 +4,8 @@
 
 extern keymap_config_t keymap_config;
 
+#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)| MOD_BIT(KC_RSHIFT))
+
 #define QWERTY 0
 #define LOWER 1
 #define UPPER 2
@@ -14,17 +16,21 @@ enum {
   VOLD_MUTE = 2
 };
 
+enum function_id {
+  SHIFT_UP,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWERTY] = {
     {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
     {CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
-    {TD(SFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, TG(NORMAN) },
+    {TD(SFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  F(3), TG(NORMAN) },
     {MO(LOWER), KC_LALT, KC_LGUI, F(1),  XXXXXXX, XXXXXXX, XXXXXXX, F(2),  KC_RGUI, KC_LEFT,  KC_DOWN,  KC_RIGHT }
   },
   [LOWER] = {
-    {LSFT(KC_GRV),  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL  },
+    {S(KC_GRV),  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL  },
     {_______, _______, _______, _______, _______,  _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE },
-    {_______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, LSFT(KC_SLSH), _______ },
+    {_______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, S(KC_SLSH), _______ },
     {_______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______ }
   },
   [UPPER] = {
@@ -49,6 +55,8 @@ const uint16_t PROGMEM fn_actions[] = {
 
   // Tap for space, hold for L2
   [2] = ACTION_LAYER_TAP_KEY(UPPER, KC_SPC),
+
+  [3] = ACTION_FUNCTION(SHIFT_UP),
 };
 
 void persistant_default_layer_set(uint16_t default_layer) {
@@ -60,3 +68,30 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [SFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
   [VOLD_MUTE] = ACTION_TAP_DANCE_DOUBLE(KC_VOLD, KC_MUTE)
 };
+
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+  static uint8_t shift_esc_shift_mask;
+  switch (id) {
+    case SHIFT_UP:
+      shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
+      if (record->event.pressed) {
+        if (shift_esc_shift_mask) {
+          add_key(KC_QUES);
+          send_keyboard_report();
+        } else {
+          add_key(KC_UP);
+          send_keyboard_report();
+        }
+      } else {
+        if (shift_esc_shift_mask) {
+          del_key(KC_QUES);
+          send_keyboard_report();
+        } else {
+          del_key(KC_UP);
+          send_keyboard_report();
+        }
+      }
+      break;
+  }
+}
