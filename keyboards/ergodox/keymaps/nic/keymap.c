fe03252386c0141a9ba7b8f4c92f0cad3a191007
Nic Aitch <nic@nicinabox.com>
2017-06-20 20:03:24 -0500

    Tweak ergodox layout

--- keyboards/ergodox/keymaps/nic/keymap.c
+++ keyboards/ergodox/keymaps/nic/keymap.c
@@ -9,21 +9,24 @@
 #define NUMPAD 3
 #define NAV 4
 
+#define CTL_ESC CTL_T(KC_ESC)
+#define HPR_TAB ALL_T(KC_TAB)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [DEFAULT] = KEYMAP(
-  KC_EQUAL,           KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   KC_ESCAPE,
-  KC_TAB,             KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   TG(NORMAN),
+  KC_EQUAL,           KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   KC_ESC,
+  HPR_TAB,            KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   KC_CAPSLOCK,
   KC_LCTL,            KC_A,     KC_S,     KC_D,     KC_F,   KC_G,
-  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   ALL_T(KC_GRAVE),
-  KC_CAPSLOCK,        _______,  KC_LALT,  KC_LGUI,  MO(LOWER),
+  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   KC_GRAVE,
+  _______,            TG(NORMAN),  KC_LALT,  KC_LGUI,  MO(LOWER),
 
                                           KC_HOME,  KC_END,
                                                     KC_PGUP,
                                 KC_BSPACE,   F(1),  KC_PGDOWN,
 
 
-    TG(NUMPAD),      KC_6,      KC_7,     KC_8,      KC_9,      KC_0,      KC_MINUS,
+    _______,      KC_6,      KC_7,     KC_8,      KC_9,      KC_0,      KC_MINUS,
     KC_LBRC,         KC_Y,      KC_U,     KC_I,      KC_O,      KC_P,      KC_RBRC,
                      KC_H,      KC_J,     KC_K,      KC_L,      KC_SCOLON, KC_QUOTE,
     MEH_T(KC_BSLS),  KC_N,      KC_M,     KC_COMMA,  KC_DOT,    KC_SLASH,  KC_LSHIFT,
@@ -103,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [NAV] = KEYMAP(
-  RESET,  _______,_______,_______,_______,_______,_______,
-  M(0),   _______,_______,_______,_______,_______,_______,
+  _______,  _______,_______,_______,_______,_______,RESET,
+  _______,   _______,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,
