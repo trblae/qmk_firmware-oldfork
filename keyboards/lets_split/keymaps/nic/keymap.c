69d8c2f261d8f4bbb72f40eb9e8e81cf52526dd0
Albert Co <trblae@gmail.com>
2017-07-16 19:03:01 -0400

    Adds a lot of personalizations on top of nicinabox's layout.
    - Add more personal touches for specific keys.
    - Makes spacing in layer layouts more aligned and add comment for split.
    - Adds mod keys to various layers.
    - Add PSCR, SLCK, PAUS keys.
    - Adds TODOs for future work.

--- keyboards/lets_split/keymaps/nic/keymap.c
+++ keyboards/lets_split/keymaps/nic/keymap.c
@@ -10,13 +10,13 @@ extern keymap_config_t keymap_config;
 #define UPPER_LAYER 2
 #define SPACEFN_LAYER 3
 #define TENKEY_LAYER 4
-#define NORMAN_LAYER 5
+#define GAMEPAD_LAYER 5
 #define UNDERGLOW_LAYER 6
 
 // Tap Dance
 enum {
   SFT_CAPS = 0,
-  MPLY_MUTE = 2,
+  MPLY_MUTE,
 };
 
 // Macros
@@ -26,54 +26,60 @@ enum macro_id {
   M_RANDLETTER,
 };
 
+// Albert customizations list TODO
+// Use different media buttons?
+// Replace NORMAN layer with something I would actually find more useful...
+// Maybe have bottom left key on right hand side be just space and nothing else?
+// Find a place to add print screen (KC_PSCR) and pause (KC_PAUS) and maybe scroll lock (KC_SLCK)?
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [QWERTY_LAYER] = KEYMAP( \
-  KC_TAB,            KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC, \
-  CTL_T(KC_ESC),     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-  TD(SFT_CAPS),      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  MT(MOD_RSFT, KC_ENT), \
-  MO(UNDERGLOW_LAYER),  KC_MEH,   KC_LALT,  KC_LGUI,  F(1),     F(3),     F(3),     F(2),  MT(MOD_RGUI, KC_LEFT),  KC_DOWN,  KC_UP,    KC_RGHT \
+  KC_TAB,              KC_Q,   KC_W,    KC_E,    KC_R, KC_T, /**/ KC_Y,   KC_U, KC_I,                  KC_O,    KC_P,    KC_BSPC, \
+  CTL_T(KC_ESC),       KC_A,   KC_S,    KC_D,    KC_F, KC_G, /**/ KC_H,   KC_J, KC_K,                  KC_L,    KC_SCLN, KC_QUOT, \
+  TD(SFT_CAPS),        KC_Z,   KC_X,    KC_C,    KC_V, KC_B, /**/ KC_N,   KC_M, KC_COMM,               KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT), \
+  MO(UNDERGLOW_LAYER), KC_MEH, KC_LALT, KC_LGUI, F(1), F(3), /**/ KC_SPC, F(2), MT(MOD_RGUI, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 [UPPER_LAYER] = KEYMAP( \
-  KC_GRV,                KC_1,       KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,           KC_0,     _______, \
-  _______,               KC_4,       KC_5,     KC_6,     KC_DOT,   _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,        KC_RBRC,  KC_BSLS, \
-  _______,               KC_7,       KC_8,     KC_9,     KC_0,     _______,  _______,  _______,  _______,  KC_DOT,        _______,  _______, \
-  TG(NORMAN_LAYER),     KC_HYPR,    _______,  _______,  _______,  _______,  _______,  _______,  TD(MPLY_MUTE),  KC_VOLD,  KC_VOLU,  KC_MFFD \
+  KC_GRV,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ KC_6,    KC_7,    KC_8,          KC_9,    KC_0,    _______, \
+  _______,           KC_4,    KC_5,    KC_6,    KC_DOT,  _______, /**/ _______, KC_MINS, KC_EQL,        KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______,           KC_7,    KC_8,    KC_9,    KC_0,    _______, /**/ KC_PSCR, KC_SLCK, KC_PAUS,       KC_DOT,  _______, _______, \
+  TG(GAMEPAD_LAYER), KC_HYPR, _______, KC_LGUI, _______, _______, /**/ KC_MUTE, _______, TD(MPLY_MUTE), KC_VOLD, KC_VOLU, _______\
 ),
 
 [LOWER_LAYER] = KEYMAP( \
-  KC_TILD,         KC_EXLM,   KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  _______, \
-  _______,         KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE, \
-  _______,         KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  _______, \
-  TG(TENKEY_LAYER),  KC_HYPR,   _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END \
+  KC_TILD,          KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  /**/ KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+  _______,          KC_F1,   KC_F2,   KC_F3,   KC_F4,   /**/ KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  KC_LSFT,          KC_F7,   KC_F8,   KC_F9,   KC_F10,  /**/ KC_F11,  KC_F12,  _______, _______, _______, _______, _______, \
+  TG(TENKEY_LAYER), KC_HYPR, KC_LALT, _______, _______, /**/ _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END \
 ),
 
 [SPACEFN_LAYER] = KEYMAP( \
-  _______,           _______,  M(M_USERNAME),    _______, LGUI(LSFT(KC_4)), LGUI(LCTL(LSFT(KC_4))),  _______, KC_HOME,           KC_UP,   KC_END,   _______, KC_DELETE, \
-  _______,           _______,  M(M_RANDDIGIT),   _______, _______,           _______,                KC_PGUP, KC_LEFT,           KC_DOWN, KC_RIGHT, _______, _______, \
-  _______,           _______,  M(M_RANDLETTER),  _______, _______,           _______,                KC_PGDN, LGUI(LSFT(KC_M)),  _______, _______,  _______, RESET, \
-  _______,  _______,  _______,          _______, _______,           _______,                _______, _______,           _______, _______,  _______, _______ \
+  _______, _______, M(M_USERNAME),   _______, LGUI(LSFT(KC_4)), LGUI(LCTL(LSFT(KC_4))), /**/  KC_PGUP, KC_HOME,          KC_UP,   KC_END,   _______, KC_INS, \
+  _______, _______, M(M_RANDDIGIT),  _______, _______,          _______,                /**/  KC_PGDN, KC_LEFT,          KC_DOWN, KC_RIGHT, _______, KC_DELETE, \
+  KC_LSFT, _______, M(M_RANDLETTER), _______, _______,          _______,                /**/  _______, LGUI(LSFT(KC_M)), _______, _______,  _______, _______, \
+  _______, KC_LCTL, KC_LALT,         _______, _______,          _______,                /**/  _______, _______,          KC_MPLY, KC_MSTP,  KC_MPRV, KC_MNXT \
 ),
 
 [TENKEY_LAYER] = KEYMAP( \
-  _______, _______,  _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS, KC_BSPC, \
-  _______, _______,  _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, KC_NLCK, \
-  _______, _______,  _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PDOT, KC_ENT, \
-  _______, _______,  _______, _______, _______, _______, _______, KC_KP_0, _______, _______, _______, _______ \
+  _______, _______, _______, _______, _______, _______, /**/ KC_PSLS, KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS, KC_BSPC, \
+  _______, _______, _______, _______, _______, _______, /**/ KC_PAST, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, _______, \
+  _______, _______, _______, _______, _______, _______, /**/ _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, KC_ENT, \
+  _______, _______, _______, _______, _______, _______, /**/ KC_NLCK, KC_KP_0, KC_KP_0, KC_PDOT, _______, _______ \
 ),
 
-[NORMAN_LAYER] = KEYMAP( \
-  _______,  KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,    KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN,  _______, \
-  _______,  KC_A,    KC_S,    KC_E,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_I,    KC_O,    KC_H,     KC_QUOT, \
-  _______,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P,    KC_M,    KC_COMM, KC_DOT,  _______,  KC_ENT, \
-  _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+[GAMEPAD_LAYER] = KEYMAP( \
+  _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______ \
 ),
 
 [UNDERGLOW_LAYER] = KEYMAP( \
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
-  _______,  RGB_TOG,  RGB_MOD,  RGB_HUD,  RGB_HUI,  RGB_SAD,  RGB_SAI,  RGB_VAD,  RGB_VAI,  _______,  _______,  _______, \
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______ \
+  _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, RESET,   _______, \
+  _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______, \
+  _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, /**/ RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______ \
 ),
 };
 
@@ -109,7 +115,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
   switch(id) {
     case M_USERNAME:
       if (record->event.pressed) {
-        SEND_STRING("nicinabox");
+        SEND_STRING("albert");
       }
       break;
 
