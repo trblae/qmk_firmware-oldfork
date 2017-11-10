777fbc1ba9a32990b317f64145362a73912c19a3
Albert Co <trblae@gmail.com>
2017-11-10 18:10:42 -0500

    Fix gamepad layer

--- keyboards/lets_split/keymaps/nic/keymap.c
+++ keyboards/lets_split/keymaps/nic/keymap.c
@@ -40,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [UPPER_LAYER] = KEYMAP( \
-  KC_GRV,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ KC_6,    KC_7,    KC_8,          KC_9,    KC_0,    _______, \
-  _______,           KC_4,    KC_5,    KC_6,    KC_DOT,  _______, /**/ _______, KC_MINS, KC_EQL,        KC_LBRC, KC_RBRC, KC_BSLS, \
-  KC_MEH,            KC_7,    KC_8,    KC_9,    KC_0,    _______, /**/ KC_PSCR, KC_SLCK, KC_PAUS,       KC_DOT,  _______, _______, \
-  TG(GAMEPAD_LAYER), KC_HYPR, _______, KC_LGUI, _______, _______, /**/ KC_MUTE, _______, TD(MPLY_MUTE), KC_VOLD, KC_VOLU, KC_RCTL \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ KC_6,              KC_7,    KC_8,          KC_9,    KC_0,    _______, \
+  _______, KC_4,    KC_5,    KC_6,    KC_DOT,  _______, /**/ _______,           KC_MINS, KC_EQL,        KC_LBRC, KC_RBRC, KC_BSLS, \
+  KC_MEH,  KC_7,    KC_8,    KC_9,    KC_0,    _______, /**/ KC_PSCR,           KC_SLCK, KC_PAUS,       KC_DOT,  _______, KC_MUTE, \
+  _______, KC_HYPR, _______, KC_LGUI, _______, _______, /**/ TG(GAMEPAD_LAYER), _______, TD(MPLY_MUTE), KC_VOLD, KC_VOLU, KC_RCTL \
 ),
 
 [LOWER_LAYER] = KEYMAP( \
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, /**/ KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, \
   KC_ESC,  KC_A, KC_S, KC_D, KC_F, KC_G, /**/ KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_J, KC_COMM, KC_DOT, KC_SLSH, TD(RSHIFT_ENT), \
-  KC_LCTL, _______, KC_LALT, KC_LGUI, _______, KC_SPC, /**/ KC_SPC, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
+  KC_LCTL, _______, KC_LALT, KC_LGUI, _______, KC_SPC, /**/ _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
 ),
 
 [UNDERGLOW_LAYER] = KEYMAP( \
