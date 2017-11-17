0459fe595e41a4a12111ed7c6d1677b04bab76e8
Albert Co <trblae@gmail.com>
2017-11-17 00:34:43 -0500

    Clean up and special tap dances
    - Remove verbosity for avrdude.

--- keyboards/lets_split/keymaps/nic/keymap.c
+++ keyboards/lets_split/keymaps/nic/keymap.c
@@ -19,7 +19,6 @@ enum {
   MPLY_MUTE,
   GUI_LEFT,
   RSHIFT_ENT,
-  RSHIFT_ENT_4EVER,
 };
 
 // Macros
@@ -57,29 +56,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [UPPER_LAYER] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ KC_6,               KC_7,    KC_8,          KC_9,    KC_0,    _______, \
   _______, KC_4,    KC_5,    KC_6,    KC_DOT,  _______, /**/ _______,            KC_MINS, KC_EQL,        KC_LBRC, KC_RBRC, KC_BSLS, \
-  KC_MEH,  KC_7,    KC_8,    KC_9,    KC_0,    _______, /**/ KC_PSCR,            KC_SLCK, KC_PAUS,       KC_DOT,  _______, KC_MUTE, \
-  _______, KC_HYPR, KC_LGUI, _______, _______, _______, /**/ M(M_GAMEPAD_LAYER), _______, TD(MPLY_MUTE), KC_VOLD, KC_VOLU, KC_RCTL \
+  KC_HYPR, KC_7,    KC_8,    KC_9,    KC_0,    _______, /**/ KC_PSCR,            KC_SLCK, KC_PAUS,       KC_DOT,  _______, KC_MUTE, \
+  _______, KC_MEH,  KC_LGUI, _______, _______, _______, /**/ M(M_GAMEPAD_LAYER), _______, TD(MPLY_MUTE), KC_VOLD, KC_VOLU, KC_RCTL \
 ),
 
 [SPACEFN_LAYER] = KEYMAP( \
   KC_HYPR, _______,              M(M_USERNAME),   _______, LGUI(LSFT(KC_4)), LGUI(LCTL(LSFT(KC_4))), /**/  KC_PGUP, KC_HOME,          KC_UP,   KC_END,   _______, KC_INS, \
   _______, _______,              M(M_RANDDIGIT),  _______, _______,          _______,                /**/  KC_PGDN, KC_LEFT,          KC_DOWN, KC_RGHT,  _______, KC_DELETE, \
   KC_LSFT, _______,              M(M_RANDLETTER), _______, _______,          _______,                /**/  _______, LGUI(LSFT(KC_M)), _______, _______,  _______, _______, \
-  _______, M(M_UNDERGLOW_LAYER), _______,         KC_LALT, _______,          _______,                /**/  _______, _______,          KC_MPLY, KC_MSTP,  KC_MPRV, KC_MNXT \
+  _______, M(M_UNDERGLOW_LAYER), _______,         KC_LALT, _______,          _______,                /**/  KC_CALC, KC_MSEL,          KC_MPLY, KC_MSTP,  KC_MPRV, KC_MNXT \
 ),
 
 [TENKEY_LAYER] = KEYMAP( \
   _______, _______, _______, _______, _______, _______, /**/ KC_PSLS, KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS, KC_BSPC, \
-  _______, _______, _______, _______, _______, _______, /**/ KC_PAST, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, _______, \
-  _______, _______, _______, _______, _______, _______, /**/ _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, KC_ENT, \
-  M_TO0,   _______, _______, KC_LALT, _______, _______, /**/ KC_NLCK, KC_KP_0, KC_KP_0, KC_PDOT, _______, _______ \
+  _______, _______, _______, _______, _______, KC_QUOT, /**/ KC_PAST, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, _______, \
+  _______, _______, _______, _______, _______, KC_COMM, /**/ KC_SCLN, KC_KP_1, KC_KP_2, KC_KP_3, _______, KC_ENT, \
+  _______, M_TO0,   _______, _______, _______, _______, /**/ KC_NLCK, KC_KP_0, KC_KP_0, KC_PDOT, _______, _______ \
 ),
 
 [GAMEPAD_LAYER] = KEYMAP( \
   KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, /**/ KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, \
   KC_ESC,  KC_A, KC_S, KC_D, KC_F, KC_G, /**/ KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(RSHIFT_ENT), \
-  KC_LCTL, _______, KC_LALT, KC_LGUI, _______, KC_SPC, /**/ M_TO0, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
+  KC_LCTL, _______, _______, KC_LALT, _______, KC_SPC, /**/ M_TO0, _______, KC_1, KC_2, KC_3, KC_4 \
 ),
 
 [UNDERGLOW_LAYER] = KEYMAP( \
@@ -104,28 +103,43 @@ const uint16_t PROGMEM fn_actions[] = {
 };
 
 // what i hope to achieve with this:
-// tap the key twice, and it registers as a right shift click
-// tap the key once, then hold the key right after to activate right shift being held
-// tap the key any number of times (that isn't 2) will act as an enter press each tap
-// tap and hold the key any number of times, and hits enter each time and when held
-// will continue to press that key until released.
+// tap the key once, this registers one enter tap
+// press the key once and hold it, this activates enter being held until let go
+// tap the key twice, this registers one right shift tap
+// tap they key once, then immediately press again and hold it (double tap then hold), this activates right shift shift being held until let go
+// tap the key any number of times (that isn't 2) will act as an enter tap for each tap
+// tap the key any number of times, and on then hold it, this taps enter for each tap and then holds the enter key until let go
 //
+// right shift and enter
 // tap dance, run this function on each tap
-void dance_each(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count != 2) {
+void dance_ent_each(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count > 2) {
         register_code(KC_ENT);
         unregister_code(KC_ENT);
     }
 }
 
 // tap dance, run this funciton when sequence is finished (timeout)
-void dance_finished(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 2) {
+void dance_shift_ent_finished(qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+    case 1:
+        register_code(KC_ENT);
+        if (state->interrupted || !state->pressed) {
+            unregister_code(KC_ENT);
+        }
+        break;
+    case 2:
         register_code(KC_RSFT);
         if (state->interrupted || !state->pressed) {
             unregister_code(KC_RSFT);
         }
-    } else {
+        break;
+    default:
+        // register 1st and 2nd enter if it gets passed 2
+        register_code(KC_ENT);
+        unregister_code(KC_ENT);
+        register_code(KC_ENT);
+        unregister_code(KC_ENT);
         if (state->pressed) {
             register_code(KC_ENT);
         }
@@ -133,7 +147,7 @@ void dance_finished(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 // tap dance, run this function when resetting (on key up)
-void dance_reset(qk_tap_dance_state_t *state, void *user_data) {
+void dance_shift_ent_reset(qk_tap_dance_state_t *state, void *user_data) {
     if (state->count == 2) {
       unregister_code(KC_RSFT);
     } else {
@@ -141,12 +155,56 @@ void dance_reset(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
+// super key and left arrow
+// tap dance, run this function on each tap
+void dance_left_each(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count > 2) {
+        register_code(KC_LEFT);
+        unregister_code(KC_LEFT);
+    }
+}
+
+// tap dance, run this funciton when sequence is finished (timeout)
+void dance_rgui_left_finished(qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+    case 1:
+        register_code(KC_LEFT);
+        if (state->interrupted || !state->pressed) {
+            unregister_code(KC_LEFT);
+        }
+        break;
+    case 2:
+        register_code(KC_RGUI);
+        if (state->interrupted || !state->pressed) {
+            unregister_code(KC_RGUI);
+        }
+        break;
+    default:
+        // register 1st and 2nd left if it gets passed 2
+        register_code(KC_LEFT);
+        unregister_code(KC_LEFT);
+        register_code(KC_LEFT);
+        unregister_code(KC_LEFT);
+        if (state->pressed) {
+            register_code(KC_LEFT);
+        }
+    }
+}
+
+// tap dance, run this function when resetting (on key up)
+void dance_rgui_left_reset(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 2) {
+      unregister_code(KC_RGUI);
+    } else {
+      unregister_code(KC_LEFT);
+    }
+}
+
 qk_tap_dance_action_t tap_dance_actions[] = {
   [SFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
   [MPLY_MUTE] = ACTION_TAP_DANCE_DOUBLE(KC_MPLY, KC_MUTE),
-  [GUI_LEFT] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT, KC_RGUI),
-  [RSHIFT_ENT] = ACTION_TAP_DANCE_DOUBLE(KC_ENT, KC_RSFT),
-  [RSHIFT_ENT_4EVER] = ACTION_TAP_DANCE_FN_ADVANCED(dance_each, dance_finished, dance_reset)
+  [GUI_LEFT] = ACTION_TAP_DANCE_FN_ADVANCED(dance_left_each, dance_rgui_left_finished, dance_rgui_left_reset),
+  [RSHIFT_ENT] = ACTION_TAP_DANCE_FN_ADVANCED(dance_ent_each, dance_shift_ent_finished, dance_shift_ent_reset)
 };
 
 // This bit of logic seeds a wee linear congruential random number generator
