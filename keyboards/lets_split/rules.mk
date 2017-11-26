a86c8ad31ff3ce37054895a36ad2effb4e84590b
Albert Co <trblae@gmail.com>
2017-11-26 12:44:26 -0500

    Decrease verbosity of auto avrdude

--- keyboards/lets_split/rules.mk
+++ keyboards/lets_split/rules.mk
@@ -83,6 +83,6 @@ avrdude: build
 	  ls /dev/tty* > /tmp/2; \
 	  USB=`diff /tmp/1 /tmp/2 | grep -o '/dev/tty.*'`; \
 	done; \
-	avrdude -v -v -v -p $(MCU) -c avr109 -P $$USB -U flash:w:$(BUILD_DIR)/$(TARGET).hex
+	avrdude -v -p $(MCU) -c avr109 -P $$USB -U flash:w:$(BUILD_DIR)/$(TARGET).hex
 
 .PHONY: avrdude
